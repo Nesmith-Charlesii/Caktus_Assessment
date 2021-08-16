ede36db937f372916219dbe4f83a6d1c5d2577dc
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-16 18:27:15 -0400

    added styling to index 'base.html'. defined get request to query for all clues in DB and display a random clue as 'context' to base.html

--- xword_data/views.py
+++ xword_data/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import View
+import random
+from .models import Clue, Puzzle, Entry
 
 
 # Create your views here.
 class DrillView(View):
     def get(self, request):
-        return render(request, 'base.html')
+        clues = Clue.objects.all()
+        display_clue = random.choice(clues)
+        print(display_clue.clue_text)
+        context = {
+            'display_clue': display_clue
+        }
+        return render(request, 'base.html', context)
