5763943e85e1eaa24ef97d69895ee1041a27880c
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-16 23:51:36 -0400

    added else statement to post method in DrillView to render base.html with same data if input does not match entry text

--- xword_data/urls.py
+++ xword_data/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = 'xword_data'
 urlpatterns = [
     path('', DrillView.as_view(), name="index"),
-    path('clue_reveal/<int:entry_id>/', DrillView.as_view(), name="reveal_answer"),
+    path('clue_reveal/<int:entry_id>/<int:clue_id>/<int:puzzle_id>', DrillView.as_view(), name="reveal_answer"),
     path('answer_reveal/', AnswerView.as_view(), name="answer_reveal")
 ]
--- xword_data/views.py
+++ xword_data/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 from django.views import View
 import random
 from .models import Clue, Puzzle, Entry
@@ -23,11 +24,21 @@ class DrillView(View):
         }
         return render(request, 'base.html', context)
 
-    def post(self, request, entry_id):
+    def post(self, request, entry_id, clue_id, puzzle_id):
         guess = request.POST['guess']
         entry = Entry.objects.get(id=entry_id)
+        puzzle = Puzzle.objects.get(id=puzzle_id)
+        clue = Clue.objects.get(id=clue_id)
         if guess == entry.entry_text:
             return HttpResponseRedirect(reverse('xword_data:answer_reveal'))
+        else:
+            messages.error(request, 'Answer is incorrect. Give it another go!')
+            context = {
+                'display_clue': clue,
+                'entry': entry,
+                'puzzle': puzzle
+            }
+            return render(request, 'base.html', context)
 
 
 class AnswerView(View):
