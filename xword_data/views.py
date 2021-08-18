6b273fc505cb1afd203602d2f04656fae6f5d529
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-18 02:48:22 -0400

    refactored nested for-loop of GET method in AnswerView to check with if statements the 'hold_entry_list' list length and values. If entry_x.entry_text is in the hold_entry_list, count adds 1. If entry is not in the list, count starts at 1

--- xword_data/views.py
+++ xword_data/views.py
@@ -31,7 +31,7 @@ class DrillView(View):
         puzzle = Puzzle.objects.get(id=puzzle_id)
         clue = Clue.objects.get(id=clue_id)
         if guess == entry.entry_text:
-            return HttpResponseRedirect(f'/crossword_drill/correct_answer_reveal/{entry.id}/{clue.id}/{puzzle.id}')
+            return redirect(f'/crossword_drill/correct_answer_reveal/{entry.id}/{clue.id}/{puzzle.id}')
         else:
             messages.error(request, 'Answer is incorrect. Give it another go!')
             context = {
@@ -58,27 +58,36 @@ def escape_hatch(request, entry_id, clue_id, puzzle_id):
 
 class AnswerView(View):
     def get(self, request, entry_id, clue_id, puzzle_id):
-        entry = Entry.objects.get(id=entry_id)
+        entry_ = Entry.objects.get(id=entry_id)
         puzzle = Puzzle.objects.get(id=puzzle_id)
         clue = Clue.objects.get(id=clue_id)
         clues = Clue.objects.filter(clue_text=clue.clue_text)
         entries = Entry.objects.all()
         hold_entry_dict = {}
         hold_entry_list = []
-        count = -1
+        count = 0
         for clue in clues:
-            for entry in entries:
-                if entry.id == clue.entry:
-                    count = count + 1
-                    hold_entry_dict[entry.entry_text] = count
-                    hold_entry_list.append(entry.entry_text)
+            for entry_x in entries:
+                if entry_x.id == clue.entry:
+                    if len(hold_entry_list) == 0:
+                        hold_entry_list.append(entry_x.entry_text)
+                        count = count + 1
+                        hold_entry_dict[entry_x.entry_text] = count
+                    elif len(hold_entry_list) > 0:
+                        if entry_x.entry_text in hold_entry_list:
+                            hold_entry_list.append(entry_x.entry_text)
+                            count = count + 1
+                            hold_entry_dict[entry_x.entry_text] = count
+                        elif entry_x.entry_text not in hold_entry_list:
+                            hold_entry_list.append(entry_x.entry_text)
+                            count = 1
+                            hold_entry_dict[entry_x.entry_text] = count
         context = {
             'display_clue': clue,
-            'entry': entry,
             'puzzle': puzzle,
             'clues': clues,
+            'entry_': entry_,
             'entries': hold_entry_dict,
-            'entry_list': hold_entry_list,
         }
         print(context['entries'])
         print(hold_entry_list)
